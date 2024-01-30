e606bf0c07bd5dd4bf167caaea639dec92db6cd7
2BlackCats <903036002alex@gmail.com>
2024-01-30 11:38:30 -0700

    Added content

--- git.py
+++ git.py
@@ -1,9 +1,18 @@
 from github import Github
 
 # or using an access token
-g = Github("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")
+g = Github("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")
 repo = g.get_repo("2BlackCats/test_repo")
 
 ## Complete your tasks from here
 
-print(repo)
+print(list(repo.get_branches()))
+
+pulls = repo.get_pulls()
+
+for pr in pulls:
+    print(pr.number)
+
+commit = repo.get_commits()
+for cm in commit:
+    print(cm)
