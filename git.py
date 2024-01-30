4df09f6852e7cf3373603579e04808de78e0c3b4
2BlackCats <903036002alex@gmail.com>
2024-01-30 11:10:04 -0700

    added repo code

--- git.py
+++ git.py
@@ -1,7 +1,7 @@
 from github import Github
 
 # or using an access token
-g = Github("your_access_token")
-repo = g.get_repo("username/repo")
+g = Github("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")
+repo = g.get_repo("2BlackCats/test_repo")
 
 ## Complete your tasks from here
