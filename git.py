5b78a5490d6b5897cea6e8a9998f50adf6b15216
2BlackCats <903036002alex@gmail.com>
2024-01-30 11:15:03 -0700

    Connected to repo

--- git.py
+++ git.py
@@ -1,7 +1,9 @@
 from github import Github
 
 # or using an access token
-g = Github("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")
+g = Github("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")
 repo = g.get_repo("2BlackCats/test_repo")
 
 ## Complete your tasks from here
+
+print(repo)
